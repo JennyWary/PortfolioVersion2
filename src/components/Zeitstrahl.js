95996daf32cb8c888896e18179ca13df8354faba
Jenny~ <j.bramadas@outlook.de>
2025-03-15 22:34:11 +0100

    Fixes

--- src/components/Zeitstrahl.js
+++ src/components/Zeitstrahl.js
@@ -1,35 +1,37 @@
 import React from "react";
 import "./css/Timeline.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import InfoRechts from "../components/InfoRechts";
 import InfoLinks from "../components/InfoLinks";
 
 export default function TimeLine() {
     return (
-        <div className="timeline-container">
+        <div>
+            <h1>Erfahrung</h1>
+            <div className="timeline-container">
+                <InfoRechts/>
+                <div className="timeline">
+                    <div className="timeline-dot"></div>
+                </div>
+                <div className="space"></div>
+                <div className="space"></div>
+                <div className="timeline">
+                    <div className="timeline-dot"></div>
+                </div>
+                <InfoLinks/>
+                <InfoRechts/>
+                <div className="timeline">
+                    <div className="timeline-dot"></div>
+                </div>
+                <div className="space"></div>
+                <div className="space"></div>
+                <div className="timeline">
+                    <div className="timeline-dot"></div>
+                </div>
+                <InfoLinks/>
 
-            {/*<div className="timeline-dot"></div>*/}
-            <InfoRechts/>
-            <div className="timeline">
-                <div className="timeline-dot"></div>
-            </div>
-            <div className="space"></div>
-            <div className="space"></div>
-            <div className="timeline">
-                <div className="timeline-dot"></div>
-            </div>
-            <InfoLinks/>
-            <InfoRechts/>
-            <div className="timeline">
-                <div className="timeline-dot"></div>
-            </div>
-            <div className="space"></div>
-            <div className="space"></div>
-            <div className="timeline">
-                <div className="timeline-dot"></div>
-            </div>
-            <InfoLinks/>
 
-
+            </div>
         </div>
     );
 }
