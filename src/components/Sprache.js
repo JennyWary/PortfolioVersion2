ad6779e690af07f7676a8483f456aca54e0d552f
Jenny~ <j.bramadas@outlook.de>
2025-03-16 23:35:35 +0100

    Sprache update

--- src/components/Sprache.js
+++ src/components/Sprache.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./css/sprache.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import htmlImage from "./image/html.png";
 
-export default function Sprache() {
+
+
+// Dynamische Sprach-Komponente
+export default function Sprache({ name, image, farbe }) {
     return (
-        <div className="sprache-container mt-3">
+        <div className={`sprache-container mt-3 d-flex align-items-center ${farbe}`}>
             <div className="col-4 col-md-2 text-center">
-                 <img src={htmlImage} alt="Code Beispiel" className="sprache-image"/>
+                <img src={image} alt={name} className="sprache-image"/>
             </div>
-            <div className=" sprache-text col-8 col-md-10">
-              <div className="mb-0">HTML</div>
+            <div className="sprache-text col-8 col-md-10">
+                <div className="mb-0">{name}</div>
             </div>
         </div>
     );
