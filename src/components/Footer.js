168ad11c338716fac80d8fa7beaef223ea774061
Wary <j.bramadas@outlook.de>
2025-03-18 23:07:31 +0100

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./css/Footer.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Footer() {
+    return (
+        <div className="container-f  text-center ">
+            <p className="m-0">© 2025 Jennifer Bramadas</p>
+        </div>
+    );
+}
