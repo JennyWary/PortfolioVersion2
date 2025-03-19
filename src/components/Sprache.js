9f85b1accf437cce0ae9800f1857a14301d72302
Wary <j.bramadas@outlook.de>
2025-03-19 20:29:39 +0100

    update

--- src/components/Sprache.js
+++ src/components/Sprache.js
@@ -8,12 +8,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function Sprache({ name, image, farbe }) {
     return (
         <div className={`sprache-container mt-3 d-flex align-items-center ${farbe}`}>
-            <div className="col-4 col-md-2 text-center">
+            <div className="col-4 col-md-2 d-flex justify-content-center">
                 <img src={image} alt={name} className="sprache-image"/>
             </div>
-            <div className="sprache-text col-8 col-md-10">
+            <div className="sprache-text col-8 col-md-10 d-flex justify-content-center align-items-center">
                 <div className="mb-0">{name}</div>
             </div>
+
         </div>
     );
 }
